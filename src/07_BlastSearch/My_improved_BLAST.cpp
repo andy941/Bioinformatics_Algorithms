d5cfcc3da37995d40f12d4e9f27dc89f2483a47e
andy941 <finocchio.andrea94@gmail.com>
2022-04-24 20:54:17 +0100

    ready to compile

---
+++ src/07_BlastSearch/My_improved_BLAST.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+/*
+ */
+
+using namespace std;
+
+int main() {
+
+  // AT1G02750.2
+  string query{"TTGTGTCACCATATCGACGAAGAACATCGTCATGAAGCTAACAATGGGATATGTCCTGTA"};
+}
