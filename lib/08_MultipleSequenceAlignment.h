18b0060ccae687088fb430e57388808fc765f0d5
andy941 <finocchio.andrea94@gmail.com>
2022-07-27 23:31:19 +0100

    Reafactor: keep multiple_alignment class separate

--- lib/08_MultipleSequenceAlignment.h
+++ lib/08_MultipleSequenceAlignment.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "06_SequenceAlignment.h"
+#include "08_MultipleAlignmentClass.h"
 #include <Eigen/Core>
 #include <string>
 #include <vector>
@@ -8,21 +9,6 @@
 namespace msa {
 
 typedef Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic> similarity_matrix;
-
-int sum_of_pairs(const std::string &str,
-                 std::unordered_map<std::string, int> &sm, const int gap_cost);
-
-struct multiple_alignment {
-  std::vector<std::string> aln_pos{};
-  std::vector<std::string> seq_names{};
-  int score{0};
-
-  multiple_alignment() = default;
-  void add_sequence(const std::string &name, const std::string &seq);
-  int get_score(std::unordered_map<std::string, int> &sm, const int gap_cost);
-  std::string get_consensus();
-  void print(size_t line_size = 80);
-};
 
 class msa {
   multiple_alignment m_aln{};
